3e159321eff33c006216fadb453819f6b1abfbe9
Josh Levy <joshaaronlevy@gmail.com>
2021-03-10 12:46:01 -0700

    "Cleaned up directory and added branch option. Ready to publish v.2.27.2"

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@
 const program = require('commander');
 const core = require('../lib/core.js');
 const cleanup = require('../lib/cleanup.js');
+const branch = require('../lib/branch.js');
 
 program
   .description(`Example: gitbuddy "I fixed a bug"`)
@@ -10,11 +11,15 @@ program
   .option('-p, --push', 'Automatically push to remote repository')
   .option('b, branch', 'Create new branch')
   .option('c, cleanup', 'Clean up branches')
-  .version('2.26.26', '-v, --version')
+  .version('2.27.2', '-v, --version')
   .action(async (message, command) => {
-    if (message.cleanup === true) {
+    // console.log(program);
+    if (message.cleanup === true || message.c === true) {
       command = message;
       await cleanup(command);
+    } else if (message.branch === true || message.b === true) {
+      command = message;
+      await branch(command);
     } else if (!command || command === undefined) {
       command = message;
       await core(command);
