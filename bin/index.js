1b7b79e750d42a67c809800f1edc0749f87c8a2a
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-04-01 21:46:13 -0600

    Fixed bugs

--- bin/index.js
+++ bin/index.js
@@ -2,22 +2,41 @@ import { Command } from "commander";
 const program = new Command();
 import main from "../lib/main.js";
 import branch from "../lib/branch.js";
+import commit from "../lib/commit.js";
+
+let commandOptions = {
+	flags: {
+		all: false,
+		push: false,
+		commit: false,
+		branch: false
+	},
+	commitMessage: null
+};
 
 program.description("Example: gitbuddy \"I fixed a bug\"")
 	.option("[message]", "Commit message")
 	.option("-A, -a, --all", "Stage all files")
 	.option("-p, --push", "Automatically push to remote repository")
 	.option("-b, --branch", "Manage branches")
-	.version("5.0.0", "-v, --version")
+	.option("-c, --commit", "Commit prompts")
+	.version("5.0.3", "-v, --version")
 	.action(async (message, command) => {
-		if (message.branch === true || message.b === true) {
-			command = message;
-			await branch(command);
-		} else if (!command || command === undefined) {
-			command = message;
-			await main(command);
+		if (Object.keys(command._optionValues).length > 0) {
+			Object.keys(command._optionValues).map((key) => commandOptions.flags[key] = true);
+		}
+		if (command.args.length > 0) {
+			commandOptions.commitMessage = command.args[0];
+		}
+		const includedFlags = Object.values(commandOptions.flags).filter((value) => value === true);
+		if (includedFlags.length > 0) {
+			if (commandOptions.flags.branch === true) {
+				return await branch(commandOptions);
+			} else {
+				return await commit(commandOptions);
+			}
 		} else {
-			await main(command);
+			return await main(commandOptions);
 		}
 	});
 
