96612ec11234f81b4e7b59d9c2037b300f97a22f
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-04-01 19:10:15 -0600

    Updated version

--- bin/index.js
+++ bin/index.js
@@ -8,7 +8,7 @@ program.description("Example: gitbuddy \"I fixed a bug\"")
 	.option("-A, -a, --all", "Stage all files")
 	.option("-p, --push", "Automatically push to remote repository")
 	.option("-b, --branch", "Manage branches")
-	.version("4.4.4", "-v, --version")
+	.version("5.0.0", "-v, --version")
 	.action(async (message, command) => {
 		if (message.branch === true || message.b === true) {
 			command = message;
